a080f93d3108ec6d63fd68b4ecc4c5439ed05b5a
Eyder Santiago Suarez Chavez <eyder.suarez@correounivalle.edu.co>
2025-12-07 23:32:26 -0500

    recorrido en portada

---
+++ projects/showcase/src/app/showcases/descripcion/descripcion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descripcion',
+  standalone: true,
+  imports: [],
+  templateUrl: './descripcion.component.html',
+  styleUrl: './descripcion.component.scss'
+})
+export class DescripcionComponent {
+
+}
